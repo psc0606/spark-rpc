62f39ff904b9a84898ed6e9620cfb702829a0793
pengshaocheng <psc0606@163.com>
2020-12-29 00:15:19 +0800

    merge HelloEndpoint and HelloServer

--- rpc-example/src/main/scala/org/apache/spark/example/HelloServer.scala
+++ rpc-example/src/main/scala/org/apache/spark/example/HelloServer.scala
@@ -1,11 +1,30 @@
 package org.apache.spark.example
 
 import org.apache.spark.SparkConf
-import org.apache.spark.rpc.{RpcEndpoint, RpcEnv}
+import org.apache.spark.rpc.{RpcCallContext, RpcEndpoint, RpcEnv}
 
 /**
  * @author pengshaocheng
  */
+class HelloEndpoint(override val rpcEnv: RpcEnv) extends RpcEndpoint {
+  override def onStart(): Unit = {
+    println("start hello endpoint!")
+  }
+
+  override def receiveAndReply(context: RpcCallContext): PartialFunction[Any, Unit] = {
+    case SayHi(msg) =>
+      println(s"receive $msg")
+      context.reply(s"hi, $msg")
+    case SayBye(msg) => {
+      println(s"receive $msg")
+      context.reply(s"bye, $msg")
+    }
+  }
+}
+
+case class SayHi(msg: String)
+case class SayBye(msg: String)
+
 object HelloServer {
   def main(args: Array[String]): Unit = {
     val rpcEnv: RpcEnv = RpcEnv.create("hello-word", "localhost", 3031, new SparkConf(), null)
